bb52fcbbafc9388adffba41f99160c484a473516
Pablo Victor <vargas.pablo.victor@gmail.com>
2021-04-11 16:53:40 -0300

    11/4/21
    ultimas animaciones

--- pages/nuevoproyecto.js
+++ pages/nuevoproyecto.js
@@ -7,26 +7,26 @@ const transition = { duration: 0.6, ease: [0.43, 0.13, .023, 0.96]};
 export default function Nuevoproyecto() {
     return (
         <Layout>
-            <motion.h1
-                className="product_title">Nuevo Proyecto</motion.h1>
-                    <motion.div 
-                        className= 'product_container'
-                        >
-                            <Link href="/about">
-                                <a >
-                                    <motion.img
-                                        className="product_image"
-                                        src="/zumba.jpg"
-                                        initial={{scale: 0, x: -2000}}
-                                        animate={{scale: 1, transition: transition, x: 0}}
-                                        />
-                                </a>
-                            </Link>
-                            <motion.p className="product_text"
-                            initial={{scale: 0}}
-                            animate={{transition: transition, scale: 1}}
-                            >Con el campeón mundial de taekwondo Gustavo Oyarzo hemos desarrollado el proyecto "Armonía" ; un gimnasio itinerante al aire libre que combina una disciplina fuerte y desendrenada como puede ser el Kick-boxing, Hit , Boxeo o Taekwondo con el YOGA para finalizar nuestras prácticas con paz mental y fisíca es decir en perfecta ARMOÍA con nuestro ser y entorno; para ver más y asistir a nuestro proyecto puedes contactarnos por instagram @Anyluisavargas <a target="_blank" href="https://www.instagram.com/gus_oyarzo/">@Gus_Oyarzo</a> <a href="https://www.instagram.com/goteambarcelona/" target="_blank">@goteambarcelona</a> y te resolveremos cualquier duda.</motion.p>
-                    </motion.div>
+            <motion.div className= 'product_container'>
+                    <Link href="/about">
+                        <a>
+                            <motion.img
+                                className="product_image"
+                                src="/zumba.jpg"
+                                initial={{scale: 0, x: -2000}}
+                                animate={{scale: 1, transition: transition, x: 0}}
+                                />
+                        </a>
+                    </Link>
+                    <div className="product_container-right">
+                        <motion.h1
+                            className="product_title">Nuevo Proyecto</motion.h1>
+                        <motion.p className="product_text"
+                        initial={{scale: 0}}
+                        animate={{transition: transition, scale: 1}}
+                        >Con el campeón mundial de taekwondo Gustavo Oyarzo hemos desarrollado el proyecto "Armonía" ; un gimnasio itinerante al aire libre que combina una disciplina fuerte y desendrenada como puede ser el Kick-boxing, Hit , Boxeo o Taekwondo con el YOGA para finalizar nuestras prácticas con paz mental y fisíca es decir en perfecta ARMOÍA con nuestro ser y entorno; para ver más y asistir a nuestro proyecto puedes contactarnos por instagram @Anyluisavargas <a target="_blank" href="https://www.instagram.com/gus_oyarzo/">@Gus_Oyarzo</a> <a href="https://www.instagram.com/goteambarcelona/" target="_blank">@goteambarcelona</a> y te resolveremos cualquier duda.</motion.p>
+                    </div>
+            </motion.div>
         </Layout>
     )
 }
--- pages/salsayzumba.js
+++ pages/salsayzumba.js
@@ -2,21 +2,28 @@ import Link from "next/link";
 import Layout from '../components/Layout';
 import { motion } from 'framer-motion';
 
+const transition = { duration: 0.6, ease: [0.43, 0.13, .023, 0.96]};
+
 export default function Salsayzumba() {
     return (
         <Layout>
-            <motion.h1 
-            className="product_title">Salsa y Zumba</motion.h1>
                 <motion.div className= 'product_container'>
                     <Link href="/about">
                         <a>
                             <motion.img
                                 className="product_image"
                                 src="/salsa.jpg"
+                                initial={{scale: 0, x: -2000}}
+                                animate={{scale: 1, transition: transition, x: 0}}
                                 />
                         </a>
                     </Link>
-                        <p className="product_text">Fue a los 24 años en España, Alicante, que comencé a bailar Salsa Caleña a nivel profesional con el grupo Sabor Dinámico , muy reconocido a nivel mundial... y ZUMBA es una disciplina que combina los ritmos latinos y el fitness, y que me ayuda a conectar y hacer conectar a personas que quizás nunca han bailado, con la música y sus cuerpos a través de la danza.</p>
+                    <div className="product_container-right">
+                        <motion.h1 className="product_title">Salsa y Zumba</motion.h1>
+                        <motion.p className="product_text"
+                        initial={{scale: 0}}
+                        animate={{transition: transition, scale: 1}}>Fue a los 24 años en España, Alicante, que comencé a bailar Salsa Caleña a nivel profesional con el grupo Sabor Dinámico , muy reconocido a nivel mundial... y ZUMBA es una disciplina que combina los ritmos latinos y el fitness, y que me ayuda a conectar y hacer conectar a personas que quizás nunca han bailado, con la música y sus cuerpos a través de la danza.</motion.p>
+                    </div>
                 </motion.div>
         </Layout>
     )
