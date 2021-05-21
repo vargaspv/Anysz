19bb51e53bfb1793f38058d04fcc65d25a130d6c
Pablo Victor <vargas.pablo.victor@gmail.com>
2021-05-21 20:38:51 -0300

    arreglos mios
    arreglando el boton en responsive

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,7 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import bootstrap from 'bootstrap'
 
 function MyApp({ Component, pageProps, router }) {
     return (
--- profile.js
+++ profile.js
@@ -1,34 +1,34 @@
-// Archivo de configuracion
+ // Archivo de configuracion
 
-export const comentarios = [{
-        nombre: 'Antonio',
-        comentario: 'Que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original.'
-    },
-    {
-        nombre: 'Laura',
-        comentario: 'Excelente tus clases ani.'
-    },
-    {
-        nombre: 'Agustina',
-        comentario: 'Tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original.'
-    },
-]
+ export const comentarios = [{
+         nombre: 'Antonio',
+         comentario: 'Que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original.'
+     },
+     {
+         nombre: 'Laura',
+         comentario: 'Excelente tus clases ani.'
+     },
+     {
+         nombre: 'Agustina',
+         comentario: 'Tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original.'
+     },
+ ]
 
-export const actividades = [{
-    id: 0,
-    title: 'Salsa y Zumba',
-    description: 'Fue a los 24 años en España, Alicante, que comencé a bailar Salsa Caleña a nivel profesional con el grupo Sabor Dinámico , muy reconocido a nivel mundial... y ZUMBA es una disciplina que combina los ritmos latinos y el fitness, y que me ayuda a conectar y hacer conectar a personas que quizás nunca han bailado, con la música y sus cuerpos a través de la danza.',
-}, {
-    id: 1,
-    title: 'Yoga',
-    description: 'En el año 2019 el universo confuyó para que hiciera ese tan soñado viaje a India... y alli partí en busqueda  de la sabiduria ancestral del Yoga...a sus ríces, sus inicios..a aprender con los verdaderos YOGUIS la  Filosofia del Yoga, Ayurveda, Hatha Yoga , Ashtanga Yoga, Yoga Nidra, anatomía del Yoga...para poder un día como hoy transmitirte algo de paz mental y conexión con el propio ser. ..tal como se me fue transmitida en su momento y en cada prática diaria a mí... en muestra de mi gratitud , a la Vida,  al Yoga y a mis Maestros de Rishikul Yogshala en RishiKesh, India. Espero poder ayudarte con mi Dharma, Gracias por leer esta expresión de mi humilde experiencia, Namasté.',
-    id: 1
-}, {
-    id: 2,
-    title: 'Nuevo Proyecto',
-    description: 'Con el campeón mundial de taekwondo Gustavo Oyarzo hemos desarrollado el proyecto "Armonía" ; un gimnasio itinerante al aire libre que combina una disciplina fuerte y desendrenada como puede ser el Kick-boxing, Hit , Boxeo o Taekwondo con el YOGA para finalizar nuestras prácticas con paz mental y fisíca es decir en perfecta ARMOÍA con nuestro ser y entorno; para ver más y asistir a nuestro proyecto puedes contactarnos por instagram @Anyluisavargas @Gus_Oyarzo @goteambarcelona y te resolveremos cualquier duda.',
-}, {
-    id: 3,
-    title: 'Sobre Mi',
-    description: 'Mi experiencia con los Bailes Latinos comienza en mi infancia... en mi ciudad natal Comodoro Rivadavia, Chubut , Argentina...desde muy pequeñita con 8 años comencé a bailar en el Grupo de aerobica de competición de Sandra Varela, mi mentora y maestra de vida, en el cual recorriamos diferentes ritmos y disciplinas, Hip Hop, Funky , Ritmos Latinos, Step y Aeróbica de competición... fue a los 24 años en España, Alicante, que comencé a bailar Salsa Caleña a nivel profesional con el grupo Sabor Dinámico , muy reconocido a nivel mundial, después de este despegue en el mundo de la salsa me transladé a Milan , Italia, donde seguí bailando a nivel profesional con bailarines brillantes de reconocimiento internacional,  diferentes estilos de salsa, Los Angeles, y On2, Ladies Style y en diversos congresos de Salsa y Bachata. Toda mi trayectoria de Salsa la continué paralelamente al Fitness, con entrenos personales y clases dirigidas. He participado , creado y co-creado eventos de ZUMBA en diferentes países de Europa, agredeida por poder transmitir, la alegria y la conexión que nos aporta la danza a todos, sin distinguir géneros , razas, niveles de baile...ni nada... conexión con el propio SER...En la actualidad, paralela a mi trabajo en el club "Bonasport" uno de los clubs míticos ,más prestigiosos de Barcelona, ocupo mi tiempo libre en diferentes proyectos creativos , con la finalidad de hacer un bien mayor a la sociedad...',
-}]
+ export const actividades = [{
+     id: 0,
+     title: 'Salsa y Zumba',
+     description: 'Fue a los 24 años en España, Alicante, que comencé a bailar Salsa Caleña a nivel profesional con el grupo Sabor Dinámico , muy reconocido a nivel mundial... y ZUMBA es una disciplina que combina los ritmos latinos y el fitness, y que me ayuda a conectar y hacer conectar a personas que quizás nunca han bailado, con la música y sus cuerpos a través de la danza.',
+ }, {
+     id: 1,
+     title: 'Yoga',
+     description: 'En el año 2019 el universo confuyó para que hiciera ese tan soñado viaje a India... y alli partí en busqueda  de la sabiduria ancestral del Yoga...a sus ríces, sus inicios..a aprender con los verdaderos YOGUIS la  Filosofia del Yoga, Ayurveda, Hatha Yoga , Ashtanga Yoga, Yoga Nidra, anatomía del Yoga...para poder un día como hoy transmitirte algo de paz mental y conexión con el propio ser. ..tal como se me fue transmitida en su momento y en cada prática diaria a mí... en muestra de mi gratitud , a la Vida,  al Yoga y a mis Maestros de Rishikul Yogshala en RishiKesh, India. Espero poder ayudarte con mi Dharma, Gracias por leer esta expresión de mi humilde experiencia, Namasté.',
+     id: 1
+ }, {
+     id: 2,
+     title: 'Nuevo Proyecto',
+     description: 'Con el campeón mundial de taekwondo Gustavo Oyarzo hemos desarrollado el proyecto "Armonía" ; un gimnasio itinerante al aire libre que combina una disciplina fuerte y desendrenada como puede ser el Kick-boxing, Hit , Boxeo o Taekwondo con el YOGA para finalizar nuestras prácticas con paz mental y fisíca es decir en perfecta ARMOÍA con nuestro ser y entorno; para ver más y asistir a nuestro proyecto puedes contactarnos por instagram @Anyluisavargas @Gus_Oyarzo @goteambarcelona y te resolveremos cualquier duda.',
+ }, {
+     id: 3,
+     title: 'Sobre Mi',
+     description: 'Mi experiencia con los Bailes Latinos comienza en mi infancia... en mi ciudad natal Comodoro Rivadavia, Chubut , Argentina...desde muy pequeñita con 8 años comencé a bailar en el Grupo de aerobica de competición de Sandra Varela, mi mentora y maestra de vida, en el cual recorriamos diferentes ritmos y disciplinas, Hip Hop, Funky , Ritmos Latinos, Step y Aeróbica de competición... fue a los 24 años en España, Alicante, que comencé a bailar Salsa Caleña a nivel profesional con el grupo Sabor Dinámico , muy reconocido a nivel mundial, después de este despegue en el mundo de la salsa me transladé a Milan , Italia, donde seguí bailando a nivel profesional con bailarines brillantes de reconocimiento internacional,  diferentes estilos de salsa, Los Angeles, y On2, Ladies Style y en diversos congresos de Salsa y Bachata. Toda mi trayectoria de Salsa la continué paralelamente al Fitness, con entrenos personales y clases dirigidas. He participado , creado y co-creado eventos de ZUMBA en diferentes países de Europa, agredeida por poder transmitir, la alegria y la conexión que nos aporta la danza a todos, sin distinguir géneros , razas, niveles de baile...ni nada... conexión con el propio SER...En la actualidad, paralela a mi trabajo en el club "Bonasport" uno de los clubs míticos ,más prestigiosos de Barcelona, ocupo mi tiempo libre en diferentes proyectos creativos , con la finalidad de hacer un bien mayor a la sociedad...',
+ }]
