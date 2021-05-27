eefbe30c4646ff2479952f10f07568d76e6f40ad
Pablo Victor <vargas.pablo.victor@gmail.com>
2021-05-27 15:58:55 -0300

    Update _app.js
    agregando script js de bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -20,7 +20,9 @@ function MyApp({ Component, pageProps, router }) {
                 0, minimum-scale=1.0" />
                 <meta charSet="utf-8"/>
                 <meta name="keywords" content="clases de baile, zumba, bachata, yoga, barcelona, españa"/>
-                <link rel="icon" href="/logo.png"   />
+                <link rel="icon" href="/logo.png"   /> 
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+        
                 <title>Anysz</title>
             </Head>
             <AnimateSharedLayout >
